3e23f76c95d5979e891dc46be71cc847bbeaede2
duynk13 <khuongduyprobg@gmail.com>
2022-07-04 21:10:37 +0700

    sf

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ if __name__ == '__main__':
         model = MultiCalibModel(opt, x_train, y_train, lab_train, x_val, y_val, lab_val, x_test, y_test, lab_test, baseline=1)
     elif opt.use_n == 11:
         from multicalib import MultiCalibModel
-        model = MultiCalibModel(opt, x_train, y_train, lab_train, x_val, y_val, lab_val, x_test, y_test, lab_test, baseline=1)
+        model = MultiCalibModel(opt, x_train, y_train, lab_train, x_val, y_val, lab_val, x_test, y_test, lab_test, baseline=2)
     elif opt.use_n == 2:
         from multicalib import MultiCalibModel
         model = MultiCalibModel(opt, x_train, y_train, lab_train, x_val, y_val, lab_val, x_test, y_test, lab_test)
